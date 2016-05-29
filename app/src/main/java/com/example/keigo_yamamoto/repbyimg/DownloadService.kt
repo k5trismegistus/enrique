9e6ae82a3d445e7447ffc094e523bed81bf419e1
keigo_yamamoto <keigo_yamamoto@dwango.co.jp>
2016-05-29 11:59:11 +0900

    Notification

--- app/src/main/java/com/example/keigo_yamamoto/repbyimg/DownloadService.kt
+++ app/src/main/java/com/example/keigo_yamamoto/repbyimg/DownloadService.kt
@@ -1,7 +1,10 @@
 package com.example.keigo_yamamoto.repbyimg
 
 import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
@@ -19,13 +22,23 @@ class DownloadService: Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        var intentMain = Intent(applicationContext, MainActivity::class.java)
+
+        var pendingIntent = PendingIntent
+                .getActivity(applicationContext, 0, intentMain, 0)
+
         Log.i("TestService", "onStartCommand");
         var notif= Notification.Builder(this)
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText("onStartCommand")
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentIntent(applicationContext)
-                .build();
+                .setSmallIcon(android.R.drawable.ic_menu_save)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        var notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(R.string.app_name, notif)
+
         return START_STICKY;
     }
 
